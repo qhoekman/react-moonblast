d801357196aead8c3be1f8d8c7657b853abb5f0c
Quido <quido@qhoekman.nl>
2019-04-23 21:35:41 +0200

    Added react-icon; Added Pagination control

---
+++ src/stories/navigation.stories.tsx
@@ -0,0 +1,21 @@
+import { select } from '@storybook/addon-knobs';
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import Theme from 'react-tailwhip';
+import { Color } from 'react-tailwhip/dist/theme';
+
+import Pagination from '../navigation/Pagination';
+
+const Colors = Object.keys(Theme.colors) as Color[];
+
+storiesOf('Navigation', module).add(
+  'Pagination',
+  () => (
+    <div style={{ width: 300 }}>
+      <Pagination color={select('color', Colors, 'teal')} parent={'news'} current={1} total={6} />
+    </div>
+  ),
+  {
+    info: { inline: true }
+  }
+);
