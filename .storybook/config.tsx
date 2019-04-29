5e0db94c754065af4c910da1388ffc7415430cc0
Quido Hoekman <quido@qhoekman.nl>
2019-04-29 12:00:24 +0200

    Remove chromatic

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -1,12 +1,11 @@
 import { withKnobs } from '@storybook/addon-knobs';
-import 'storybook-chromatic';
 import { addDecorator, configure } from '@storybook/react';
 import { ThemeProvider } from 'emotion-theming';
 import React from 'react';
 import Theme from 'react-tailwhip';
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../src/stories', true, /\.stories\.tsx$/);
+const req = require.context("../src/stories", true, /\.stories\.tsx$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
