ada238a45573d331c878ce1c1e4adaee10482551
Quido <quido@qhoekman.nl>
2019-07-30 12:07:38 +0200

    Added more buttons

--- packages/storybook/src/data-renderers.stories.tsx
+++ packages/storybook/src/data-renderers.stories.tsx
@@ -1,6 +1,6 @@
-import { PrimaryButton } from '@moonblast/buttons';
+import { PrimaryButton, SecondaryButton, TertiaryButton } from '@moonblast/buttons';
 import { Badge } from '@moonblast/data-renderers';
-import { Round, Styled } from '@moonblast/style';
+import { Round } from '@moonblast/style';
 import { boolean, number, select, text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
@@ -14,32 +14,35 @@ const Rounded = ['sm', 'md', 'full'] as Round[];
 storiesOf('Data Renderers', module).add(
   'Badge',
   () => {
-    const Icons = {
+    const Counts = {
       Clock: <FaRegClock />,
       Number: 5
     };
-    const componentName = select('count', Object.keys(Icons), 'Number');
-    const Box = Styled('div')(({ theme }) => ({
-      verticalAlign: 'middle',
-      padding: `${theme.padding[3]} ${theme.padding[4]}`,
-      border: `${theme.borderWidths.xs} solid ${theme.colors.greyLight}`,
-      borderRadius: theme.borderRadius.sm,
-      display: 'inline-block'
-    }));
+    const Boxes = {
+      PrimaryButton: PrimaryButton,
+      SecondaryButton: SecondaryButton,
+      TertiaryButton: TertiaryButton
+    };
+    const countComponent = select('count', Object.keys(Counts), 'Number');
+    const boxComponent = select('box', Object.keys(Boxes), 'PrimaryButton');
+    const Box = Boxes[boxComponent];
 
     return (
       <div style={{ padding: 20 }}>
         <Badge
-          count={Icons[componentName]}
+          count={Counts[countComponent]}
           dot={boolean('dot', false)}
           bgColor={select<Color>('bgColor', Colors, 'red')}
           color={select<Color>('color', Colors, 'white')}
           showZero={boolean('showZero', true)}
           overflowCount={number('overflowCount', 99)}
         >
-          <PrimaryButton color={'white'} background={'blue'}>
+          <Box
+            color={select<Color>('boxColor', Colors, 'red')}
+            background={select<Color>('boxBgColor', Colors, 'blueDark')}
+          >
             {text('label', 'Hello world')}
-          </PrimaryButton>
+          </Box>
         </Badge>
       </div>
     );
